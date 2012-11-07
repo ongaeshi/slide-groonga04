046f613f6e354d22d49bcef46457e82f0e79dd64
ongaeshi <ongaeshi0621@gmail.com>
2012-11-08 00:10:16 +0900

    要らない要素等を削除

--- slide/slide_config.js
+++ slide/slide_config.js
@@ -1,14 +1,14 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
+    title: 'rroongaを使ったソースコード検索エンジン Milkode',
+    // subtitle: 'コンソールアプリにも便利',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    //favIcon: 'images/google_developers_logo_tiny.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
@@ -18,12 +18,12 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title, Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: 'ongaeshi',
+    // company: '',
+    // gplus: 'http://plus.google.com/1234567890',
+    twitter: '@ongaeshi',
+    www: 'http://ongaeshi.me',
+    github: 'http://github.com/ongaeshi'
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
